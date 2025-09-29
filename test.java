dfa2a96da9048078d899cf0bc2f851146a7695b9
bapeters826 <bapeters@mtu.edu>
2025-09-29 16:02:43 -0400

    Revert "Update test.java"
    This reverts commit 34a030bd0f038941a1dafb77c9ca3a303864b5e2.

--- test.java
+++ test.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         // test comment for commit
         System.out.println("Hello, world!");
-        bug to be reverted
     }
 }
